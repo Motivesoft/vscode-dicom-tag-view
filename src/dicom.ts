91450d83698a5a259c554b298a63de1b218c3e79
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-11-02 12:09:18 +0100

    Start to deal with sequences

--- src/dicom.ts
+++ src/dicom.ts
@@ -42,10 +42,15 @@ export class DicomReadonlyEditorProvider implements vscode.CustomReadonlyEditorP
         const content = await vscode.workspace.fs.readFile(uri);
         return dicomParser.parseDicom( content );
     }
-
+    
     private getHtmlForWebview(webview: vscode.Webview, dicomContent: dicomParser.DataSet): string {
         const nonce = this.getNonce();
 
+/* 
+        let output : string[] = [];
+        dumpDataSet(dicomContent.elements, output);
+        const formattedDicom = output.join('');
+ */ 
         let formattedDicom = "";
         const elements = dicomContent.elements;
         for (let tag in elements) {
@@ -55,7 +60,20 @@ export class DicomReadonlyEditorProvider implements vscode.CustomReadonlyEditorP
 
           let escapedValue: string;
           if (typeof value === 'undefined') {
-            escapedValue = "[undefined]";
+            if (vr === "SQ" ) {
+                escapedValue = "[sequence]";
+                if( element.items ) {
+                    element.items.forEach( item => {
+                        //formattedDicom+=`<br>&nbsp;&nbsp;&nbsp;&nbsp;${item.tag} : ${item.vr} : ${dicomParser.explicitElementToString(dicomContent,item)}`;
+                        //formattedDicom+=`<br>&nbsp;&nbsp;&nbsp;&nbsp;${item.tag} : ${item.vr}`;
+                        if( item.dataSet ) {
+                            formattedDicom += this.getHtmlForWebview(webview, item.dataSet);
+                        }
+                    });
+                }
+            } else {
+                escapedValue = "[undefined]";
+            }
           } else {
             escapedValue = this.escapeHtml(value);
           }
@@ -69,7 +87,7 @@ export class DicomReadonlyEditorProvider implements vscode.CustomReadonlyEditorP
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>JSON Viewer</title>
+                <title>DICOM Tag Viewer</title>
                 <style>
                     body {
                         font-family: Arial, sans-serif;
@@ -110,3 +128,57 @@ export class DicomReadonlyEditorProvider implements vscode.CustomReadonlyEditorP
             .replace(/'/g, "&#039;");
     }
 }
+
+/*     
+    // This function iterates through dataSet recursively and adds new HTML strings
+    // to the output array passed into it
+    function dumpDataSet(instance : {[tag: string]: dicomParser.Element} | undefined, output : string[])
+    {
+        // the dataSet.elements object contains properties for each element parsed.  The name of the property
+        // is based on the elements tag and looks like 'xGGGGEEEE' where GGGG is the group number and EEEE is the
+        // element number both with lowercase hexadecimal letters.  For example, the Series Description DICOM element 0008,103E would
+        // be named 'x0008103e'.  Here we iterate over each property (element) so we can build a string describing its
+        // contents to add to the output array
+        for(var propertyName in instance) {
+            var element = instance[propertyName];
+            
+            // The output string begins with the element tag, length and VR (if present).  VR is undefined for
+            // implicit transfer syntaxes
+            var text = propertyName;
+            //text += " length=" + element.length;
+
+            if(element.constructor === Array) {
+                output.push('<li>' + text + '</li>');
+                output.push('<ul>');
+
+                let elementArray : Array<dicomParser.Element> = element;
+
+                // each item contains its own data set so we iterate over the items
+                // and recursively call this function
+                var itemNumber = 0;
+                (element as Array<dicomParser.Element>).forEach(function (item) {
+                    output.push('<li>Item #' + itemNumber++ + '</li>');
+                    output.push('<ul>');
+                    dumpDataSet(item.dataSet?.elements, output);
+                    output.push('</ul>');
+                });
+                output.push('</ul>');
+            } else if(typeof element === 'object')
+            {
+                if(element.hadUndefinedLength) {
+                    text += " <strong>(-1)</strong>";
+                }
+                text += " length=" + element.length + '; offset=' + element.dataOffset;
+            } else if(typeof element === 'string') {
+                text += " = " + element;
+            }
+
+
+            var color = 'black';
+
+            // finally we add the string to our output array surrounded by li elements so it shows up in the
+            // DOM as a list
+            output.push('<li style="color:' + color +';">'+ text + '</li>');
+        }
+    }
+ */

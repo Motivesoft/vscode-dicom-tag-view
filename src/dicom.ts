ee0523ca522ca34f7e3cc29d318baed9a31b745d
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2024-11-02 13:08:27 +0100

    Working using HTML lists

--- src/dicom.ts
+++ src/dicom.ts
@@ -47,15 +47,24 @@ export class DicomReadonlyEditorProvider implements vscode.CustomReadonlyEditorP
     private getHtmlForDataSet(dataSet: dicomParser.DataSet): string {
         let formattedDicom = "";
         const elements = dataSet.elements;
-        for (let tag in elements) {
+
+        // Display in tag order
+        var keys = [];
+        for(let tag in dataSet.elements) {
+            keys.push(tag);
+        }
+        keys.sort();
+        
+        for (let index in keys) {
+            const tag = keys[index];
             const element = elements[tag];
             const vr = element.vr;
             const value = dicomParser.explicitElementToString(dataSet, element);
 
-            let escapedValue: string;
+            formattedDicom += `<li>${this.formatTag(tag)} : ${vr} : `;
             if (typeof value === 'undefined') {
                 if (vr === "SQ") {
-                    formattedDicom += `<li>${tag} : ${vr} : [sequence, item count = ${element.items?.length}]</li>`;
+                    formattedDicom += `[sequence, item count = ${element.items?.length}]`;
                     if (element.items) {
                         let index = 0;
                         element.items.forEach(item => {
@@ -66,11 +75,12 @@ export class DicomReadonlyEditorProvider implements vscode.CustomReadonlyEditorP
                         });
                     }
                 } else {
-                    formattedDicom += `<li>${tag} : ${vr} : [undefined]</li>`;
+                    formattedDicom += `[undefined]`;
                 }
             } else {
-                formattedDicom += `<li>${tag} : ${vr} : ${this.escapeHtml(value)}</li>`;
+                formattedDicom += `${this.escapeHtml(value)}`;
             }
+            formattedDicom += `</li>`;
         }
 
         return formattedDicom;
@@ -132,58 +142,14 @@ export class DicomReadonlyEditorProvider implements vscode.CustomReadonlyEditorP
             .replace(/"/g, "&quot;")
             .replace(/'/g, "&#039;");
     }
-}
 
-/*     
-    // This function iterates through dataSet recursively and adds new HTML strings
-    // to the output array passed into it
-    function dumpDataSet(instance : {[tag: string]: dicomParser.Element} | undefined, output : string[])
-    {
-        // the dataSet.elements object contains properties for each element parsed.  The name of the property
-        // is based on the elements tag and looks like 'xGGGGEEEE' where GGGG is the group number and EEEE is the
-        // element number both with lowercase hexadecimal letters.  For example, the Series Description DICOM element 0008,103E would
-        // be named 'x0008103e'.  Here we iterate over each property (element) so we can build a string describing its
-        // contents to add to the output array
-        for(var propertyName in instance) {
-            var element = instance[propertyName];
-            
-            // The output string begins with the element tag, length and VR (if present).  VR is undefined for
-            // implicit transfer syntaxes
-            var text = propertyName;
-            //text += " length=" + element.length;
+    private formatTag(tagValue : string ) : string {
+        // Remove the first character (x) and split the rest into two parts
+        const numbers = tagValue.slice(1);
+        const firstPart = numbers.slice(0, 4);
+        const secondPart = numbers.slice(4);
 
-            if(element.constructor === Array) {
-                output.push('<li>' + text + '</li>');
-                output.push('<ul>');
-
-                let elementArray : Array<dicomParser.Element> = element;
-
-                // each item contains its own data set so we iterate over the items
-                // and recursively call this function
-                var itemNumber = 0;
-                (element as Array<dicomParser.Element>).forEach(function (item) {
-                    output.push('<li>Item #' + itemNumber++ + '</li>');
-                    output.push('<ul>');
-                    dumpDataSet(item.dataSet?.elements, output);
-                    output.push('</ul>');
-                });
-                output.push('</ul>');
-            } else if(typeof element === 'object')
-            {
-                if(element.hadUndefinedLength) {
-                    text += " <strong>(-1)</strong>";
-                }
-                text += " length=" + element.length + '; offset=' + element.dataOffset;
-            } else if(typeof element === 'string') {
-                text += " = " + element;
-            }
-
-
-            var color = 'black';
-
-            // finally we add the string to our output array surrounded by li elements so it shows up in the
-            // DOM as a list
-            output.push('<li style="color:' + color +';">'+ text + '</li>');
-        }
+        // Join the parts with a comma
+        return `${firstPart},${secondPart}`;
     }
- */
+}
